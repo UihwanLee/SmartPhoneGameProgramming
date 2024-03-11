a2601d2f155c8b75221c22015c68534a83aeb3cb
UihwanLee <luh5063014@naver.com>
2024-03-11 16:30:54 +0900

    [2주차] Button 컴포넌트 입력을 받아 Text 표시하기

--- SampleJavaApp/app/src/main/java/kr/ac/tukorea/luh/uihwan/simpleandroidapp/MainActivity.java
+++ SampleJavaApp/app/src/main/java/kr/ac/tukorea/luh/uihwan/simpleandroidapp/MainActivity.java
@@ -3,16 +3,26 @@ package kr.ac.tukorea.luh.uihwan.simpleandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = (Button)findViewById(R.id.pushMeButton);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("Tag", "msg");
         TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("Just Loaded");
+        tv.setText("Button pushed");
     }
 }
