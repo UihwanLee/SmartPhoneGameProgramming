e4beddd927566b86e498a398bc23a79666e247c0
UihwanLee <luh5063014@naver.com>
2024-03-11 17:23:43 +0900

    [2주차] TextView를 멤버 변수로 활용하여 connection 최적화 구현

--- SampleJavaApp/app/src/main/java/kr/ac/tukorea/luh/uihwan/simpleandroidapp/MainActivity.java
+++ SampleJavaApp/app/src/main/java/kr/ac/tukorea/luh/uihwan/simpleandroidapp/MainActivity.java
@@ -9,19 +9,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView nameTextView;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTextView = findViewById(R.id.nameTextView);
     }
 
     public void onBtnPushMe(View view) {
-        TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("Push Me");
+        nameTextView.setText("Push Me");
     }
 
     public void onBtnAnother(View view) {
-        TextView tv = findViewById(R.id.nameTextView);
-        tv.setText("Another");
+        nameTextView.setText("Another");
     }
 }
