0d1a235bdbb22831f617cdd8e698cb30cc23da49
UihwanLee <luh5063014@naver.com>
2024-03-22 13:04:29 +0900

    [3주차] 보이는 카드가 없을 때 재시작

--- Cards/app/src/main/java/kr/ac/tukorea/luh/uihwan/cards/MainActivity.java
+++ Cards/app/src/main/java/kr/ac/tukorea/luh/uihwan/cards/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             iv.setTag(resIds[i]);
         }
         setFlips(0);
+        visiblePairCount = VIEW_IDS.length / 2;
     }
 
     public void onBtnCard(View view) {
@@ -73,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
                 imageButton.setVisibility(View.INVISIBLE);
                 previousCardImageButton.setVisibility(View.INVISIBLE);
                 previousCardImageButton = null;
+                visiblePairCount -= 1;
+                if(visiblePairCount == 0) {
+                    askRestart();
+                }
                 return;
             }
             previousCardImageButton.setImageResource(R.mipmap.card_blue_back);
@@ -85,7 +90,9 @@ public class MainActivity extends AppCompatActivity {
         setFlips(flips + 1);
     }
 
-    int flips;
+    private int flips;
+    private int visiblePairCount;
+
     private void setFlips(int flips) {
         this.flips = flips;
         String text = String.format("Flips: %d", flips);
@@ -93,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnRestart(View view) {
-        //startGame();
         askRestart();
     }
 
